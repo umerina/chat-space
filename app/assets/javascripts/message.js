10926bf96ab0221f4abce0322a3db52d56c1cc5e
umerina <61220531+umerina@users.noreply.github.com>
2020-03-04 17:55:26 +0900

    send

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -56,6 +56,10 @@ $('#new_message').on('submit', function(e){
     $('.chat-main__message-list').append(html);      
     $('form')[0].reset();
     $('.chat-main__message-list').animate({ scrollTop: $('.chat-main__message-list')[0].scrollHeight});
+    $('.form__send').prop('disabled', false);
   })
+  .fail(function(){
+  alert('メッセージ送信に失敗しました');
 })
 });
+
