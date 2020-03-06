6dc215abdb684fad571d40afb229248a648a1391
umerina <61220531+umerina@users.noreply.github.com>
2020-03-06 11:37:10 +0900

    pic

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -77,6 +77,7 @@ $('#new_message').on('submit', function(e){
 });
   var reloadMessages = function() {
   var last_message_id = $('.message:last').data("message-id");
+  console.log(last_message_id)
   $.ajax({
     url: "api/messages",
     type: 'get',
@@ -84,14 +85,21 @@ $('#new_message').on('submit', function(e){
     data: {id: last_message_id}
   })
   .done(function(messages) {
+    if (messages.length !== 0)  {
     var insertHTML = '';
     $.each(messages, function(i, message) {
       insertHTML += buildHTML(message)
     });
-    $('.messages').append(insertHTML);
+    $('.chat-main__message-list').append(insertHTML);
+    $('.chat-main__message-list').animate({ scrollTop: $('.chat-main__message-list')[0].scrollHeight});
+  }
   })
   .fail(function() {
     alert('error');
   });
   };
-})
+  setInterval(reloadMessages, 7000);
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
+});
